5b952e61400a6ff0b9d0e72636fe1602e12f085b
Yato <yatoakii@gmail.com>
2023-05-27 21:36:33 +0630

    build: data retrived from json

--- src/Components/QSection.tsx
+++ src/Components/QSection.tsx
@@ -1,10 +1,34 @@
 
 import React from "react";
+import {useState, useEffect} from "react"
+import dataFile from "../data.json"
+
+type Choice = {
+    id: string;
+    value: string;
+};
+  
+type Question = {
+    id: string;
+    question: string;
+    choices: Choice[];
+    correct_answer: string;
+};
+
+type QuestionData = Question[];
+  
 
 const QSection: React.FC = () => {
+
+    const [data, setData] = useState<QuestionData | null>()
+
+    useEffect(()=> {
+        setData(dataFile)
+    },[])
+
     return(
         <div>
-            hi
+            
         </div>
     )
 } 
