9e89507835576c89e8d2ba1835b9c26a0623f8b4
Yato <yatoakii@gmail.com>
2023-05-27 19:09:23 +0630

    feat: added Home page UI

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import "./Home.css"
 
 const Home: React.FC = () => {
     return(
-        <div>
-            hi
+        <div className="Home">
+            <h1>Quiz Game</h1>
+            <div className="centerIcon">🎲</div>
+            <p>
+                Play Now and Unleash <br/> Your Inner Quiz Master!
+            </p>
+            <div className="inputArea">
+                <input type="text" placeholder="Enter your name" />
+                <button>START</button>
+            </div>
         </div>
     )
 }
