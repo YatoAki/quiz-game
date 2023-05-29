824e624021903d650f9ac1c6b8e11ee2939d5d41
Yato <yatoakii@gmail.com>
2023-05-29 22:19:46 +0630

    feat: added Result UI

--- src/Components/Result.tsx
+++ src/Components/Result.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Result.css"
 
 interface CustomStyle extends React.CSSProperties {
     "--value": number;
@@ -6,8 +7,31 @@ interface CustomStyle extends React.CSSProperties {
 const Result: React.FC = () => {
 
     return(
-        <div>
-            <div className="radial-progress bg-primary text-primary-content border-4 border-primary" style={{ "--value": 70 } as CustomStyle}>70%</div>
+        <div className="Result">
+            <h1>Quiz Game</h1>
+            <p>You have completed the quiz!</p>
+            <div id="card">
+                <div className="cardHeader">
+                    <h2>Congratulation!</h2>
+                    <p>Kaung Nay Lin Khant</p>
+                </div>
+                <div className="progressContainer">
+                    <div className="scoreProgress">
+                        <div className="radial-progress bg-[var(--primaryColor)] text-[var(--white)] border-4 border-[var(--primaryColor)]" style={{ "--value": 70} as CustomStyle}>70%</div>
+                        <h3>SCORE</h3>
+                    </div>
+                    <div className="timeProgress">
+                    <div className="radial-progress bg-[var(--primaryColor)] text-[var(--white)] border-4 border-[var(--primaryColor)]" style={{ "--value": 70 } as CustomStyle}>70%</div>
+                        <h3>TIME</h3>
+                    </div>
+                </div>
+                <p>Thanks for taking part in our amazing quiz!</p>
+            </div>
+            <div className="controls">
+                <button>Save as image</button>
+                <button>Retake the Quiz</button>
+            </div>
+            
         </div>
     )
 }
